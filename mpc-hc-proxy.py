a74e9d8e181be6f9b164ad64cbe05d171b4e3c11
Dean Camera <dean@fourwalledcubicle.com>
2019-01-09 20:37:32 +1100

    Run autopep8 formatter on project soruce.

--- mpc-hc-proxy.py
+++ mpc-hc-proxy.py
@@ -35,32 +35,32 @@ VARIABLE_REDACTIONS = [
 
 # List of allowable MPC-HC commands, Commands not listed here will be rejected.
 ALLOWED_COMMAND_IDS = [
-    889, # Play/Pause
-    887, # Play
-    888, # Pause
-    890, # Stop
-    891, # Frame-step
-    892, # Frame-step back
-    893, # Go To
-    895, # Increase Rate
-    894, # Decrease Rate
-    896, # Reset Rate
-    905, # Audio Delay +10 ms
-    906, # Audio Delay -10 ms
-    900, # Jump Forward (small)
-    899, # Jump Backward (small)
-    902, # Jump Forward (medium)
-    901, # Jump Backward (medium)
-    904, # Jump Forward (large)
-    903, # Jump Backward (large)
-    898, # Jump Forward (keyframe)
-    897, # Jump Backward (keyframe)
-    996, # Jump to Beginning
-    922, # Next
-    921, # Previous
-    909, # Volume Mute
-    908, # Volume Down
-    907, # Volume Up
+    889,  # Play/Pause
+    887,  # Play
+    888,  # Pause
+    890,  # Stop
+    891,  # Frame-step
+    892,  # Frame-step back
+    893,  # Go To
+    895,  # Increase Rate
+    894,  # Decrease Rate
+    896,  # Reset Rate
+    905,  # Audio Delay +10 ms
+    906,  # Audio Delay -10 ms
+    900,  # Jump Forward (small)
+    899,  # Jump Backward (small)
+    902,  # Jump Forward (medium)
+    901,  # Jump Backward (medium)
+    904,  # Jump Forward (large)
+    903,  # Jump Backward (large)
+    898,  # Jump Forward (keyframe)
+    897,  # Jump Backward (keyframe)
+    996,  # Jump to Beginning
+    922,  # Next
+    921,  # Previous
+    909,  # Volume Mute
+    908,  # Volume Down
+    907,  # Volume Up
 ]
 
 
@@ -77,21 +77,19 @@ class MPCHC_Proxy_Client(object):
         app.router.add_get('/status.html', self.handle_page_status)
         app.router.add_get('/command.html', self.handle_page_command)
 
-
     async def _send_command(self, command_id):
         if self.session is None:
             self.session = aiohttp.ClientSession()
 
         try:
             data = {
-                "wm_command" : command_id
+                "wm_command": command_id
             }
 
             with aiohttp.Timeout(1):
                 await self.session.get('http://127.0.0.1:{}/command.html'.format(self.port), params=data)
         except Exception as e:
             pass
-
 
     async def _get_variables(self):
         if self.session is None:
@@ -113,7 +111,6 @@ class MPCHC_Proxy_Client(object):
 
         return mpchc_variables_parsed
 
-
     async def _render_template(self, page_template, template_values=None):
         mpchc_variables = await self._get_variables()
 
@@ -121,23 +118,23 @@ class MPCHC_Proxy_Client(object):
             template_values = dict()
 
         template_values.update({
-            "{FILE}"           : mpchc_variables.get("file"          , ""),
-            "{FILEPATHARG}"    : mpchc_variables.get("filepatharg"   , ""),
-            "{FILEPATH}"       : mpchc_variables.get("filepath"      , ""),
-            "{FILEDIRARG}"     : mpchc_variables.get("filedirarg"    , ""),
-            "{FILEDIR}"        : mpchc_variables.get("filedir"       , ""),
-            "{STATE}"          : mpchc_variables.get("state"         , "0"),
-            "{STATESTRING}"    : mpchc_variables.get("statestring"   , "Stopped"),
-            "{POSITION}"       : mpchc_variables.get("position"      , "0"),
-            "{POSITIONSTRING}" : mpchc_variables.get("positionstring", "00:00:00"),
-            "{DURATION}"       : mpchc_variables.get("duration"      , "0"),
-            "{DURATIONSTRING}" : mpchc_variables.get("durationstring", "00:00:00"),
-            "{VOLUMELEVEL}"    : mpchc_variables.get("volumelevel"   , "0"),
-            "{MUTED}"          : mpchc_variables.get("muted"         , "0"),
-            "{PLAYBACKRATE}"   : mpchc_variables.get("playbackrate"  , "1"),
-            "{SIZE}"           : mpchc_variables.get("size"          , "1"),
-            "{RELOADTIME}"     : mpchc_variables.get("reloadtime"    , "0"),
-            "{VERSION}"        : mpchc_variables.get("version"       , "1.0.0.0"),
+            "{FILE}": mpchc_variables.get("file", ""),
+            "{FILEPATHARG}": mpchc_variables.get("filepatharg", ""),
+            "{FILEPATH}": mpchc_variables.get("filepath", ""),
+            "{FILEDIRARG}": mpchc_variables.get("filedirarg", ""),
+            "{FILEDIR}": mpchc_variables.get("filedir", ""),
+            "{STATE}": mpchc_variables.get("state", "0"),
+            "{STATESTRING}": mpchc_variables.get("statestring", "Stopped"),
+            "{POSITION}": mpchc_variables.get("position", "0"),
+            "{POSITIONSTRING}": mpchc_variables.get("positionstring", "00:00:00"),
+            "{DURATION}": mpchc_variables.get("duration", "0"),
+            "{DURATIONSTRING}": mpchc_variables.get("durationstring", "00:00:00"),
+            "{VOLUMELEVEL}": mpchc_variables.get("volumelevel", "0"),
+            "{MUTED}": mpchc_variables.get("muted", "0"),
+            "{PLAYBACKRATE}": mpchc_variables.get("playbackrate", "1"),
+            "{SIZE}": mpchc_variables.get("size", "1"),
+            "{RELOADTIME}": mpchc_variables.get("reloadtime", "0"),
+            "{VERSION}": mpchc_variables.get("version", "1.0.0.0"),
         })
 
         page_data = str(page_template)
@@ -145,7 +142,6 @@ class MPCHC_Proxy_Client(object):
             page_data = page_data.replace(k, v)
 
         return page_data
-
 
     async def handle_page_root(self, request):
         TEMPLATE = '''<!DOCTYPE html>
@@ -161,7 +157,6 @@ class MPCHC_Proxy_Client(object):
 
         page_text = await self._render_template(TEMPLATE)
         return web.Response(text=page_text, content_type='text/html')
-
 
     async def handle_page_variables(self, request):
         TEMPLATE = '''<!DOCTYPE html>
@@ -194,7 +189,6 @@ class MPCHC_Proxy_Client(object):
         page_text = await self._render_template(TEMPLATE)
         return web.Response(text=page_text, content_type='text/html')
 
-
     async def handle_page_info(self, request):
         TEMPLATE = '''<!DOCTYPE html>
 <html lang="en">
@@ -210,13 +204,11 @@ class MPCHC_Proxy_Client(object):
         page_text = await self._render_template(TEMPLATE)
         return web.Response(text=page_text, content_type='text/html')
 
-
     async def handle_page_status(self, request):
         TEMPLATE = '''OnStatus("{FILE}", "{STATESTRING}", {POSITION}, "{POSITIONSTRING}", {DURATION}, "{DURATIONSTRING}", {MUTED}, {VOLUMELEVEL}, "{FILE}")'''
 
         page_text = await self._render_template(TEMPLATE)
         return web.Response(text=page_text)
-
 
     async def handle_page_command(self, request):
         TEMPLATE = '''<!DOCTYPE html>
